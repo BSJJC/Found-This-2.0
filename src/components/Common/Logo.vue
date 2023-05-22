d4580079e529f000af69fbbaa417c9233a2b95df
BSJJC <805086021@qq.com>
2023-05-22 11:20:30 +0800

    add Common folder

---
+++ src/components/Common/Logo.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+
+interface config {
+  enableGoToHomePage: boolean;
+}
+
+const props = defineProps<config>();
+const router = useRouter();
+
+function goToHomePage() {
+  if (props.enableGoToHomePage) {
+    router.push("/");
+  }
+}
+</script>
+
+<template>
+  <div
+    class="h-full flex justify-center items-center select-none drop-shadow-lg"
+    @click="goToHomePage"
+  >
+    <div class="text-[#7e56da81]">Found</div>
+    <div class="text-[#7E56DA] font-bold mx-2">this</div>
+    <div class="text-[#7e56da81]">!</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+* {
+  cursor: pointer;
+}
+</style>
