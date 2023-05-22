75b79f5679c0adbc8c96ce5bf3a609e1b8b1f134
BSJJC <805086021@qq.com>
2023-05-22 10:03:45 +0800

    chore

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div></div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
