98a2656403958e40f298a07efb71e361b411f24d
BSJJC <805086021@qq.com>
2023-05-22 23:13:32 +0800

    chore

--- src/components/Common/TransformZoomIn.vue
+++ src/components/Common/TransformZoomIn.vue
@@ -1,15 +1,18 @@
 <template>
   <Teleport to="body">
-    <div class="flex justify-center items-center bg-gray-300 opacity-70 absolute top-0 left-0 text-white"
-      :style="{ top: `${props.states.startTop}px`, left: `${props.states.startLeft}px`, width: `${props.states.startWidth}px`, height: `${props.states.startHeight}px` }"
-      @click.self="hide">
+    <div ref="containerRef" class="w-screen h-screen flex justify-center items-center absolute top-0 left-0">
       <slot></slot>
+    </div>
+
+    <div id="mask"
+      class=" flex justify-center items-center w-screen h-screen bg-gray-500 opacity-0 absolute top-0 left-0 text-white z-5"
+      @click.self="hide">
     </div>
   </Teleport>
 </template>
   
 <script setup lang='ts'>
-import { onMounted, ref, Ref } from "vue"
+import { ref, Ref, onMounted } from "vue"
 
 interface states {
   startWidth: number;
@@ -28,7 +31,9 @@ interface config {
 
 const props = defineProps<config>()
 const emits = defineEmits(['hide']);
-const divRef: Ref = ref();
+
+const divRef: Ref<HTMLElement | undefined | null> = ref();
+const containerRef: Ref<HTMLElement | undefined> = ref();
 
 function hide(): void {
   emits('hide');
@@ -37,11 +42,46 @@ function hide(): void {
 onMounted(() => {
   divRef.value = document.getElementById(props.randomID!)
 
+  divRef.value!.style.position = "absolute"
+  divRef.value!.style.zIndex = "10"
+
+  divRef.value!.style.width = `${props.states.startWidth}px`
+  divRef.value!.style.height = `${props.states.startHeight}px`
+  divRef.value!.style.top = `${props.states.startTop}px`
+  divRef.value!.style.left = `${props.states.startLeft}px`
+
+  divRef.value!.style.borderRadius = "1rem"
 
   setTimeout(() => {
-    divRef.value.style.width = props.states.endWidth
-    divRef.value.style.height = props.states.endHeight
-  }, 1000);
+    const rect: DOMRect = containerRef.value!.getBoundingClientRect();
+
+    const top: number = (rect.height / 2) - (rect.height * 0.7 / 2)
+    const left: number = (rect.width / 2) - (rect.width * 0.8 / 2)
+
+    divRef.value!.style.top = `${top}px`
+    divRef.value!.style.left = `${left}px`
+
+    divRef.value!.style.width = `${props.states.endWidth}vw`
+    divRef.value!.style.height = `${props.states.endHeight}vh`
+
+    divRef.value!.style.fontSize = `100px`
+  }, 0);
 })
 </script>
   
+<style lang="scss" scoped>
+* {
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+@keyframes mask-in {
+  to {
+    opacity: 0.8;
+  }
+}
+
+#mask {
+  animation: mask-in .5s ease-in-out forwards;
+}
+</style>
--- src/components/Home/Navbar/components/UserIcon/index.vue
+++ src/components/Home/Navbar/components/UserIcon/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div v-if="!showTransform" ref="logInBtnRef" class="log-in-btn" @click="showLogAndSign">
+  <div v-if="!showTransform" ref="logInBtnRef"
+    class="flex justify-center items-center w-[50px] h-[50px] text-[20px] bg-red-300 text-white rounded-full cursor-pointer transition-all duration-500 ease-in-out"
+    @click="showLogAndSign">
     Log In
   </div>
 
   <template v-if="showTransform">
     <TransformZoomIn @hide="hidedTransformZoomIn" :randomID="randomID" :states="states">
-      <div :id="randomID" class="log-in-btn">
+      <div :id="randomID"
+        class="flex justify-center items-center w-[50px] h-[50px] text-[20px] bg-red-300 text-white rounded-full cursor-pointer transition-all duration-500 ">
         Log In
       </div>
     </TransformZoomIn>
@@ -17,8 +20,8 @@ import { ref, Ref } from "vue"
 import TransformZoomIn from '@/components/Common/TransformZoomIn.vue';
 
 const showTransform: Ref<boolean> = ref(false)
-const logInBtnRef = ref()
-const randomID = Math.random().toString();
+const logInBtnRef: Ref<HTMLElement | undefined> = ref()
+const randomID: string = Math.random().toString();
 const states = ref({
   startWidth: 0,
   startHeight: 0,
@@ -29,13 +32,16 @@ const states = ref({
 })
 
 function showLogAndSign(): void {
-  const rect = logInBtnRef.value.getBoundingClientRect();
+  const rect: DOMRect = logInBtnRef.value!.getBoundingClientRect();
   const { width, height, top, left } = rect;
 
   states.value.startWidth = width
   states.value.startHeight = height
   states.value.startTop = top
   states.value.startLeft = left
+
+  states.value.endWidth = 80
+  states.value.endHeight = 70
 
   showTransform.value = true
 }
@@ -47,8 +53,3 @@ function hidedTransformZoomIn(): void {
   showTransform.value = false
 };
 </script>
-  
-<style lang="sass" scoped>
-.log-in-btn 
-  @apply flex justify-center items-center w-[50px] h-[50px] text-[20px] bg-red-300 text-white rounded-full cursor-pointer transition-all duration-500 ease-in-out
-</style>
