9ce361db0fb93c9692e6299a5968dd73717d1e2b
BSJJC <805086021@qq.com>
2023-05-22 11:04:35 +0800

    BgVideo componnet

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="text-3xl text-red-300">home page</div>
+  <div class="text-3xl text-red-300">home page</div>
+  <BgVideo :video-source="a"></BgVideo>
 </template>
-  
-<script setup lang='ts'>
-  
+
+<script setup lang="ts">
+import a from "@/assets/videos/1.mp4";
+
+import BgVideo from "@/components/Effects/BgVideo.vue";
 </script>
-  
-<style>
-  
-</style>
+
+<style></style>
