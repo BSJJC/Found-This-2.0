1d408165f7c4a062656ab95e4d857cbe86de46ae
BSJJC <805086021@qq.com>
2023-05-28 17:58:25 +0800

    chore

---
+++ src/components/Home/MainBody/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    123
+  </div>
+</template>
+  
+<script setup lang='ts'>
+
+</script>
+  
+<style></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="text-3xl text-red-300">
-    <NavBar></NavBar>
+  <div class="w-screen h-screen">
+    <nav-bar class="max-h-[10%]"></nav-bar>
+
+    <main-body class="max-h-[90%]"></main-body>
 
     <BgVideo :video-source="defaulBgLight"></BgVideo>
   </div>
 </template>
 
 <script setup lang="ts">
+import NavBar from "@/components/Home/Navbar/index.vue";
+import MainBody from "@/components/Home/MainBody/index.vue"
 import defaulBgLight from "@/assets/videos/default-bg-light.mp4";
 import BgVideo from "@/components/Effects/BgVideo.vue";
-import NavBar from "@/components/Home/Navbar/index.vue";
-
 </script>
 
 <style></style>
