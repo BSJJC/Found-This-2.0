d1e7426ad7ebfebf50bb2cdb06b8b4ea6dddfce2
BSJJC <805086021@qq.com>
2023-05-29 10:17:44 +0800

    chore

---
+++ src/assets/icons/IconArrowLeft.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg :style="{
+    fill: `${props.fill}`,
+    color: `${props.color}`,
+  }" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M685.248 104.704a64 64 0 0 1 0 90.496L368.448 512l316.8 316.8a64 64 0 0 1-90.496 90.496L232.704 557.248a64 64 0 0 1 0-90.496l362.048-362.048a64 64 0 0 1 90.496 0z">
+    </path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+interface config {
+  fill?: string;
+  color?: string;
+}
+
+const props = defineProps<config>();
+</script>
--- src/components/Home/MainBody/components/Pagination.vue
+++ src/components/Home/MainBody/components/Pagination.vue
@@ -1,9 +1,63 @@
 <template>
-  123
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="flex justify-center items-center">
+      <!-- start page -->
+      <div class="flex justify-center items-center">
+        <button @mouseenter="leftArrorColor = 'white'" @mouseleave="leftArrorColor = '#7e56da'">
+          <IconArrowLeft class="h-2/3 transition-all duration-300" :fill="leftArrorColor"></IconArrowLeft>
+        </button>
+
+        <div class="flex justify-center items-center mx-1">
+          <button>1</button>
+          <transition name="ellipsis">
+            <div class="text-right w-[20px]">
+              ···
+            </div>
+          </transition>
+        </div>
+
+      </div>
+
+      <!-- middle pages -->
+      <div class="flex">
+        <div v-for="(i, index) in 9" :key="index">
+          <button>
+            {{ i }}
+          </button>
+        </div>
+      </div>
+
+      <!-- end page -->
+      <div class="flex">
+        <div class="flex justify-center items-center">
+          <transition name="ellipsis">
+            <div class="text-center w-[20px]">
+              ···
+            </div>
+          </transition>
+
+          <button class="flex justify-center items-center mx-1">1</button>
+        </div>
+
+        <button @mouseenter="rightArrorColor = 'white'" @mouseleave="rightArrorColor = '#7e56da'">
+          <IconArrowRight class="h-2/3" :fill="rightArrorColor"></IconArrowRight>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
   
 <script setup lang='ts'>
+import { ref, Ref } from "vue"
+import IconArrowLeft from '@/assets/icons/IconArrowLeft.vue';
+import IconArrowRight from '@/assets/icons/IconArrowRight.vue';
 
+const leftArrorColor: Ref<string> = ref("#7e56da")
+const rightArrorColor: Ref<string> = ref("#7e56da")
 </script>
   
-<style></style>
+<style lang="sass" scoped>
+button 
+  @apply w-[50px] h-[40px] bg-white p-2 flex justify-center items-center mx-1 text-xl rounded-md transition-all duration-300 border border-[#7e56da] text-[#7e56da] hover:border-white hover:bg-[#7e56da] hover:text-white
+
+</style>
