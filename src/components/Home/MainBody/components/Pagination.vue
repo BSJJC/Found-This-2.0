58e9626b25fc0b96ba68a19e119a253541289d2c
BSJJC <805086021@qq.com>
2023-05-29 20:21:33 +0800

    Pagination

--- src/components/Home/MainBody/components/Pagination.vue
+++ src/components/Home/MainBody/components/Pagination.vue
@@ -2,23 +2,22 @@
   <div class="w-full h-full flex justify-center items-center pb-10">
     <div class="flex justify-center items-center">
       <!-- start page -->
-      <div class="flex justify-end w-[150px] bg-red-300">
+      <div class="flex justify-end w-[150px]">
         <button @mouseenter="leftArrorColor = 'white'" @mouseleave="leftArrorColor = '#7e56da'" @click="toPrePage">
           <IconArrowLeft class="h-2/3 transition-all duration-300" :fill="leftArrorColor"></IconArrowLeft>
         </button>
 
-        <button :id="`${currentPage === 1 ? 'selected' : ''}`" @click="toSelectedPage(1)">1</button>
+        <button :id="`${currentPage === 1 ? 'selected' : ''}`" @click="toSelectedPage(1)">{{ pages[0] }}</button>
 
-        <div v-show="pageCounts > 0"
-          class="flex justify-center items-center text-center text-[#7e56da] font-bold transition-all duration-300"
-          :style="{ width: `${hasLeftHidden ? '20px' : '0px'}`, opacity: `${hasLeftHidden ? '100' : '0'}` }">
+        <div class="flex justify-center items-center text-center text-[#7e56da] font-bold transition-all duration-300"
+          :style="{ width: `${pages[1] !== 2 ? '20px' : '0px'}`, opacity: `${pages[1] !== 2 ? '100' : '0'}` }">
           ···
         </div>
       </div>
 
       <!-- middle pages -->
       <div class="flex">
-        <div v-for="(i, index) in pages" :key="index">
+        <div v-for="(i, index) in pages.slice(1, pages.length - 1)" :key="index">
           <button :id="`${i === currentPage ? 'selected' : ''}`" @click="toSelectedPage(i)">
             {{ i }}
           </button>
@@ -26,16 +25,18 @@
       </div>
 
       <!-- end page -->
-      <div class="flex justify-start w-[150px] bg-blue-300">
-        <div v-show="pageCounts > pageSize + 1"
-          class="flex justify-center items-center text-center text-[#7e56da] font-bold transition-all duration-300"
-          :style="{ width: `${hasRightHidden ? '20px' : '0px'} `, opacity: `${hasRightHidden ? '100' : '0'} ` }">
+      <div class="flex justify-start w-[150px]">
+        <div class="flex justify-center items-center text-center text-[#7e56da] font-bold transition-all duration-300"
+          :style="{
+            width: `${pages[pages.length - 2] !== pages[pages.length - 1] - 1 ? '20px' : '0px'} `,
+            opacity: `${pages[pages.length - 2] !== pages[pages.length - 1] - 1 ? '100' : '0'} `
+          }">
           ···
         </div>
 
         <button v-if="pageCounts > 1" :id="`${currentPage === pageCounts ? 'selected' : ''}`"
           @click="toSelectedPage(pageCounts)">
-          {{ pageCounts }}
+          {{ pages[pages.length - 1] }}
         </button>
 
         <button @mouseenter="rightArrorColor = 'white'" @mouseleave="rightArrorColor = '#7e56da'" @click="toNextPage">
@@ -60,24 +61,75 @@ const props = defineProps<Config>()
 
 const leftArrorColor: Ref<string> = ref("#7e56da")
 const rightArrorColor: Ref<string> = ref("#7e56da")
-const pageSize = props.pageSize;
-const pageCounts = props.pageCounts
-const pageCountsArr: number[] = []
+const pageSize: number = props.pageSize;
+const pageCounts: number = props.pageCounts
 const pages: Ref<number[]> = ref([])
-const currentPage: Ref<number> = ref(10)
-const hasLeftHidden: Ref<boolean> = ref(false)
-const hasRightHidden: Ref<boolean> = ref(false)
+const currentPage: Ref<number> = ref(1)
 
 function calculatePages(): void {
+  /**
+   * 清空所展示的所有页码
+   */
+  pages.value = []
+
+
+  /**
+   * 将pageCounts转换为数字类型的数组
+   * 例如
+   * pageCounts为10时
+   * 将其转化为[1,2,3,4,5,6,7,8,9,10]
+   * 方便后面取值
+   */
+  let pageCountsArr: number[] = [];
+  for (let i = 1; i < pageCounts + 1; i++) {
+    pageCountsArr.push(i)
+  }
+
+
+  /**
+   * 当可以一次性展示所有页码的时候
+   * 无需多余的计算
+   * 直接返回所有页码
+   */
+  if (pageCounts <= pageSize) {
+    pages.value = pageCountsArr;
+
+    return
+  }
+
+
+  /**
+   * 因为currenPage是页面上实际展示的页码
+   * 最小为1
+   * 而这里需要将其作为下标访问数组使用
+   * 所以需要-1
+   */
   const _currentPage = currentPage.value - 1;
-  let lastSize = pageSize - 1;
 
-  const leftArr = []
-  const rightArr = []
-  let direction = "left"
-  let rounds = 1
 
-  while (lastSize !== 0) {
+  /**
+   * pageSize实际上并不是实际展示的所有页码的数量
+   * 
+   * 例如
+   * 当pageSize为5，pageCounts为10时，实际上展示的所有页码为
+   * [1,2,3,4,5,6,10]
+   * 一共展示了7页
+   * 
+   * lastSize也就是指为了能展示7页，还需要添加的页码的数量
+   * 而且因为当前所展示的页面一定包含在pageSize中
+   * 所以不需要+2
+   * 只需要+1即可
+   */
+  let lastSize = pageSize + 1;
+
+
+  let result: number[] = []
+  const leftArr: number[] = []
+  const rightArr: number[] = []
+  let direction: 'left' | "right" = "left"
+  let rounds: number = 1
+
+  while (lastSize > 0) {
     if (direction === 'left') {
       const el = pageCountsArr[_currentPage - rounds]
 
@@ -109,12 +161,39 @@ function calculatePages(): void {
     }
   }
 
-
-  console.log(leftArr);
-  console.log(pageCountsArr[_currentPage]);
-  console.log(rightArr);
+  result = [...leftArr, pageCountsArr[_currentPage], ...rightArr]
 
 
+  /**
+   * 当pageSize为偶数时
+   * result的length会币pageSize+1还要大1
+   * 所以pop掉最后一个元素
+   */
+  if (result.length > pageSize + 2) {
+    result.pop()
+  }
+
+
+  /**
+   * 无论如何第一个页码必须为1
+   * 所以如果result的第一个元素部位1
+   * 则把第一个元素变成1
+   */
+  if (result[0] !== 1) {
+    result[0] = 1
+  }
+
+
+  /**
+   * 无论如何最后一个元素必须为pageCounts
+   * 所以如果最后一个元素部位pageCounts
+   * 则把最后一个元素变成pageCounts
+   */
+  if (result[result.length - 1] !== pageCounts) {
+    result[result.length - 1] = pageCounts
+  }
+
+  pages.value = result
 }
 
 function toPrePage(): void {
@@ -141,10 +220,6 @@ watch(
 )
 
 onMounted(() => {
-  for (let i = 1; i < pageCounts + 1; i++) {
-    pageCountsArr.push(i)
-  }
-
   calculatePages();
 })
 </script>
