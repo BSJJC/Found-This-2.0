f81fb2c92b47e76cc1170a6648fce1fdbc09ebd5
BSJJC <805086021@qq.com>
2023-05-31 22:52:13 +0800

    chore

--- src/components/AddTopic/EditArea/index.vue
+++ src/components/AddTopic/EditArea/index.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="w-full h-full  p-8">
+  <div class="w-full h-full flex justify-center items-start">
 
-    <el-button @click="drawer = !drawer">switch</el-button>
-    <el-drawer v-model="drawer"></el-drawer>
-
-    <Editor v-model="text" v-loading="true" api-key="tup1hjjciyq9wo6uus67ng0eemkjua5dprzb71dqrucrqh8y" class="h-[90%]"
-      :init="{
-        toolbar:
-          'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify| numlist bullist | forecolor backcolor | link codesample',
-        toolbar_sticky: false,
-        language: 'zh_CN',
-        resize: false,
-        skin: 'oxide',
-        content_css: 'light',
-      }" />
+    <div class="w-[90%] h-[90%] p-8">
+      <Editor v-model="text" v-loading="true" api-key="tup1hjjciyq9wo6uus67ng0eemkjua5dprzb71dqrucrqh8y" class="h-full"
+        :init="{
+          toolbar:
+            'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify| numlist bullist | forecolor backcolor | link codesample',
+          toolbar_sticky: false,
+          language: 'zh_CN',
+          resize: false,
+          skin: 'oxide',
+          content_css: 'light',
+        }" />
+    </div>
   </div>
 </template>
   
