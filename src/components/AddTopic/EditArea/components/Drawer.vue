47560205c21c910dd9a9b85a393c7d19e2a2f227
BSJJC <805086021@qq.com>
2023-06-01 13:35:51 +0800

    chore

--- src/components/AddTopic/EditArea/components/Drawer.vue
+++ src/components/AddTopic/EditArea/components/Drawer.vue
@@ -1,35 +1,51 @@
 <template>
-  <div class="relative w-1/2 h-full flex justify-center items-center bg-red-300 z-[500] transition-all duration-500"
+  <!-- msak -->
+  <div class="fixed top-0 left-0 w-screen h-screen z-[100] transition-all duration-500"
+    :style="{ background: `${showDrawer ? '#00000080' : '00000000'}` }" @click="show"></div>
+
+  <!-- main drawer -->
+  <div
+    class=" fixed top-0 left-0 w-1/2 h-screen flex justify-center items-center bg-white shadow-[#7e56da] shadow-2xl z-[500] transition-all duration-500"
     :style="{ transform: `${showDrawer ? 'translateX(0px)' : 'translateX(-100%)'}` }">
 
-    <div
-      class="absolute flex flex-col justify-center items-center bg-[#7e56da]  rounded-r-lg text-xl text-white cursor-pointer overflow-hidden transition-all duration-500 left-full p-4 hover:pl-[40px] top-[30%]"
-      :style="{ top: `${showDrawer ? '0px' : ''}`, height: `${showDrawer ? '80px' : '300px'}` }" @click="test">
 
-      <TransitionGroup name="op">
+
+    <!-- switch button -->
+    <div
+      class="absolute flex flex-col justify-center items-center bg-[#7e56da]  rounded-r-lg text-xl text-white cursor-pointer overflow-hidden transition-all duration-500 left-full p-4 hover:pl-[40px] top-[30%] z-[200]"
+      :style="{ top: `${showDrawer ? '0px' : ''}`, height: `${showDrawer ? '50px' : '300px'}` }" @click="show">
+
+      <TransitionGroup name="word">
         <span v-for="(char, index) in sentence" :key="index">
           {{ char }}
         </span>
       </TransitionGroup>
+
     </div>
-
-
   </div>
 </template>
   
 <script setup lang='ts'>
-import { ref, Ref } from "vue"
+import { ref, Ref, watch } from "vue"
 
-const showDrawer: Ref<boolean> = ref(false)
+const showDrawer: Ref<boolean> = ref(true)
 const sentence: Ref<string> = ref("Attachment")
 
-function test(): void {
+function show(): void {
   showDrawer.value = !showDrawer.value
-
-  sentence.value === "Attachment"
-    ? sentence.value = "X"
-    : sentence.value = "Attachment"
 }
+
+watch(
+  () => showDrawer.value,
+  () => {
+    sentence.value === "Attachment"
+      ? sentence.value = "X"
+      : sentence.value = "Attachment"
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
 <style lang="scss" scoped>
@@ -38,13 +54,13 @@ function test(): void {
   -webkit-user-select: none;
 }
 
-.op-enter-active,
-.op-leave-active {
+.word-enter-active,
+.word-leave-active {
   transition: all .3s ease-in-out;
 }
 
-.op-enter-from,
-.op-leave-to {
+.word-enter-from,
+.word-leave-to {
   opacity: 0;
 }
 </style>
