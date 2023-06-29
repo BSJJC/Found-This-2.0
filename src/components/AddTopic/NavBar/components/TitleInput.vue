210f1457010a4e20c23a0b9260d95ec230e37290
BSJJC <805086021@qq.com>
2023-06-30 02:46:19 +0800

    chore

--- src/components/AddTopic/NavBar/components/TitleInput.vue
+++ src/components/AddTopic/NavBar/components/TitleInput.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex justify-center items-center w-[700px] h-[50px] rounded-3xl border shadow-md px-10 bg-white">
-    <input v-model="title" class="w-full outline-none text-xl text-[#7E56DA]" placeholder="Topic Title" />
+    <input v-model="title" class="w-full outline-none text-xl text-[#7E56DA]" placeholder="Topic Title"
+      @input="title = disableInputSpaceByRule(title, 1)" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useNewTopicInfo } from "@/stores/useNewTopicInfo"
 import { storeToRefs } from "pinia";
+import disableInputSpaceByRule from "@/utils/disableInputSpaceByRule";
 
 const { title } = storeToRefs(useNewTopicInfo())
 </script>
