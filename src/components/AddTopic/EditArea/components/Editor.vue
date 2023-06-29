c2ef728bdf733f45357d68fdf73b5f8d54e97b3c
BSJJC <805086021@qq.com>
2023-06-30 00:36:10 +0800

    chore

--- src/components/AddTopic/EditArea/components/Editor.vue
+++ src/components/AddTopic/EditArea/components/Editor.vue
@@ -1,13 +1,25 @@
 <template>
-  <Editor v-model="text" api-key="tup1hjjciyq9wo6uus67ng0eemkjua5dprzb71dqrucrqh8y" :init="{
-    toolbar:
-      'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify| numlist bullist | forecolor backcolor | link codesample',
-    toolbar_sticky: false,
-    language: 'zh_CN',
-    resize: false,
-    skin: 'oxide',
-    content_css: 'light',
-  }" />
+  <div class="relative">
+
+    <div
+      class="absolute flex flex-col justify-center items-center text-[5rem] font-bold text-white w-full h-full bg-[#7e56da] rounded-lg">
+      <div class="animate-bounce">
+        Loading...
+      </div>
+
+      <div class="text-[2rem]">If the loading is not completed for a long time, please refresh the page 🥺</div>
+    </div>
+
+    <Editor v-model="text" class="h-full absolute" :init="{
+      toolbar:
+        'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify| numlist bullist | forecolor backcolor | link codesample',
+      toolbar_sticky: false,
+      language: 'zh_CN',
+      resize: false,
+      skin: 'oxide',
+      content_css: 'light',
+    }" />
+  </div>
 </template>
   
 <script setup lang='ts'>
@@ -17,5 +29,3 @@ import { storeToRefs } from "pinia";
 
 const { text } = storeToRefs(useNewTopicInfo())
 </script>
-  
-<style></style>
--- src/components/AddTopic/NavBar/components/Submit.vue
+++ src/components/AddTopic/NavBar/components/Submit.vue
@@ -1,18 +1,30 @@
 <template>
   <div
-    class="px-8 py-3 bg-[#7E56DA] rounded-xl text-white text-xl transition-all duration-500 cursor-pointer hover:shadow-[#7E56DA] z-[100] flex justify-center items-center">
+    class="px-8 py-3 bg-[#7E56DA] rounded-xl text-white text-xl transition-all duration-300 cursor-pointer shadow-lg hover:shadow-[#5833b0] hover:scale-105"
+    @click="submit">
     SUBMIT
   </div>
 </template>
   
 <script setup lang='ts'>
-// import { useNewTopicInfo } from "@/stores/useNewTopicInfo"
-// import { storeToRefs } from "pinia";
+import { useNewTopicInfo } from "@/stores/useNewTopicInfo"
+import { storeToRefs } from "pinia";
 
 
-// const { title, text } = storeToRefs(useNewTopicInfo())
+const { title, text } = storeToRefs(useNewTopicInfo())
+
+async function submit(): Promise<void> {
+  console.log(title.value);
+  console.log(text.value);
+
+}
 
 
 </script>
   
-<style></style>
+<style lang="scss" scoped>
+* {
+  user-select: none;
+  -webkit-user-select: none;
+}
+</style>
