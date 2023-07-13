d8df342e489df4adfbc37254e0559f1a461b88ab
BSJJC <805086021@qq.com>
2023-07-14 00:46:55 +0800

    chore

--- src/components/Home/MainBody/index.vue
+++ src/components/Home/MainBody/index.vue
@@ -1,22 +1,17 @@
 <template>
   <el-scrollbar height="100%">
-    <div class="w-full h-full flex justify-start items-center px-5 flex-wrap flex-row relative">
+    <div class="w-full h-full flex justify-start items-center flex-col px-[10%]">
       <Control></Control>
 
-      <Topics v-for="(i, index) in topicsInfo" :key="index" :data="i" :id="index" @click="zoomIn(index)"></Topics>
+      <div class="flex justify-center items-center flex-col w-full h-[200px] bg-red-300">
+        <span>
+          TODO
+        </span>
+        <span>
+          NEW topic style!
+        </span>
+      </div>
 
-      <template v-if="showTransform">
-        <Transform :randomIDs="randomIDs" :states="states">
-
-          <template #original>
-            <Topics :data="topicsInfo![selectedTopicId]" :id="randomIDs[0]"
-              class="transition-all duration-500 ease-in-out z-[100]"></Topics>
-          </template>
-
-          <template #target></template>
-
-        </Transform>
-      </template>
     </div>
 
     <Pagination :page-size="4" :page-counts="10"></Pagination>
@@ -24,77 +19,6 @@
 </template>
   
 <script setup lang='ts'>
-import { ref, Ref, defineAsyncComponent, onMounted } from "vue"
-
-import Topics from "./components/Topics.vue"
 import Pagination from "./components/Pagination.vue"
 import Control from "./components/Control.vue"
-
-const Transform = defineAsyncComponent(() => import("@/components/Common/Transform.vue"))
-
-interface Config {
-  founderName: string,
-  founderAvaterID: string,
-  title: string,
-  text: string,
-  likes: number,
-  dislikes: number,
-  replies: number,
-  views: number
-  bgID: string,
-  appendixIDs: Array<string>,
-}
-
-const topicsInfo: Ref<Config[] | undefined> = ref()
-
-const selectedTopicId = ref()
-const selectedTopicElement: Ref<HTMLElement | undefined> = ref()
-const showTransform = ref(false)
-const randomIDs: string[] = [Math.random().toString(), Math.random().toString()]
-const states = ref({
-  startWidth: 0,
-  startHeight: 0,
-  startTop: 0,
-  startLeft: 0,
-  endWidth: 0,
-  endHeight: 0
-})
-
-/**
- * 获取保存在本地的topicinfo
- */
-function getTopicInfo() {
-  setTimeout(() => {
-    topicsInfo.value = JSON.parse(sessionStorage.getItem("topicsInfo")!)
-  }, 500);
-}
-
-/**
- * 
- * @param index d
- */
-function zoomIn(index: number) {
-  selectedTopicId.value = index
-
-  selectedTopicElement.value = document.getElementById(index.toString()) as HTMLElement
-  selectedTopicElement.value!.style.opacity = "0"
-
-  const rect: DOMRect = selectedTopicElement.value.getBoundingClientRect();
-
-  const { width, height, top, left } = rect;
-
-  states.value.startWidth = width
-  states.value.startHeight = height
-  states.value.startTop = top
-  states.value.startLeft = left
-
-  states.value.endWidth = 70
-  states.value.endHeight = 70
-
-  showTransform.value = true
-}
-
-onMounted(() => {
-  getTopicInfo()
-})
 </script>
